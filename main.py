d8c2c59d3decd09e2cea64b79822d2ff592ebe25
Arman-1532 <bsse1532@iit.du.ac.bd>
2025-11-16 16:34:18 +0600

    adding github model api

--- main.py
+++ main.py
@@ -1,10 +1,35 @@
 
+from time import time
 import pyttsx3
 import webbrowser
 import speech_recognition as sr
+import requests
+import os
 
 recognizer = sr.Recognizer()
 engine = pyttsx3.init()
+newsapikey = os.environ.get("NEWS_API_KEY")
+GITHUB_TOKEN = os.environ.get("GITHUB_MODELS_TOKEN")
+API_URL = "https://models.github.ai/inference/chat/completions"
+
+def ask_github_model(prompt: str, model: str = "openai/gpt-4o"):
+    headers = {
+        "Authorization": f"Bearer {GITHUB_TOKEN}",
+        "Accept": "application/vnd.github+json",
+        "Content-Type": "application/json",
+        "X-GitHub-Api-Version": "2022-11-28"
+    }
+    body = {
+        "model": model,
+        "messages": [
+            {"role": "user", "content": prompt}
+        ]
+    }
+
+    resp = requests.post(API_URL, json=body, headers=headers)
+    resp.raise_for_status()
+    data = resp.json()    # data(dict) te id and choices(list of dict) thakbe
+    return data["choices"][0]["message"]["content"]
 
 def speak(text):
     engine.setProperty('rate', 130)
@@ -34,8 +59,23 @@ def processCommand(c):
     elif "open gmail" in c.lower():
         speak("opening gmail")
         webbrowser.open("https://www.gmail.com")
+    elif "news" in c.lower():
+        speak("top news are")
+        time.sleep(1)
+        r = requests.get(f"https://newsapi.org/v2/top-headlines?country=us&apiKey={newsapikey}")
+
+        if r.status_code == 200:
+            data = r.json()
+            articles = data["articles"]
+            for article in articles:
+                speak(article["title"]) 
+                time.sleep(1)
     else:
-        speak("command not recognized")
+       try:
+            answer = ask_github_model(c)
+            speak(answer)
+       except Exception as e:
+            print("Error calling GitHub Models API:", e)
 
 if __name__ == "__main__":
     speak("initializing nafis...")
@@ -50,6 +90,7 @@ if __name__ == "__main__":
                 audio = r.listen(source, timeout=2, phrase_time_limit=3)
             word = r.recognize_google(audio)
             print("You said: " + word)
+            print()
             
             if word.lower() == "nafis":
                 speak("yes")
